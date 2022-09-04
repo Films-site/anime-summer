25960f1eadaecc328b4f10e4902d127228e0dc82
s1veme <kpdpav@gmail.com>
2022-09-04 12:45:02 +0300

    update(anime/api): update videos field in AnimeSerializer

--- anime/api/serializers.py
+++ anime/api/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from anime.models import Anime
+from videos.api.serializers import VideoSerializer
 
 
 class AnimeSerializer(serializers.ModelSerializer):
+    videos = VideoSerializer(many=True)
+
     class Meta:
         model = Anime
-        fields = ['id', 'description', 'preview', 'videos']
+        fields = ['id', 'title', 'description', 'preview', 'videos']
