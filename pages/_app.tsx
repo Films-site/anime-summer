6175af0a4a5f853683ed9f62e7580c5a5366036c
miirsery <miirsery.main@gmail.com>
2022-12-12 00:58:49 +0700

    F-5: Add: toggle for change theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,69 @@ import AnimeLayout from '@/app/layouts/AnimeLayout/AnimeLayout'
 import { requireSvg } from '@/app/utils/loadSvg'
 
 import '@/app/styles/main.scss'
+import { useMemo, useState } from 'react'
+import {
+  ThemeProvider,
+  createTheme,
+} from '@mui/material/styles'
+import { CssBaseline, PaletteMode } from '@mui/material'
+import { ThemeOptions } from '@mui/material/styles/createTheme'
+import { ColorConstants } from '@/app/constants/color.constant'
+import { ThemeContext } from '@/app/contexts/ThemeContext'
 
 requireSvg()
 
+const getDesignTokens = (mode: PaletteMode): ThemeOptions => ({
+  palette: {
+    mode,
+    ...(mode === 'light'
+      ? {
+        // palette values for light mode
+        primary: {
+          main: ColorConstants.PRIMARY_DARK,
+        },
+        secondary: {
+          light: '#0066ff',
+          main: '#0044ff',
+          contrastText: '#ffcc00',
+        },
+      }
+      : {
+        // palette values for dark mode
+        primary: {
+          main: ColorConstants.PRIMARY,
+        },
+      }),
+  },
+})
+
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  const [mode, setMode] = useState<'light' | 'dark'>('light')
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
+      },
+    }),
+    [],
+  )
+
+  const theme = useMemo(
+    () =>
+      createTheme(getDesignTokens(mode)),
+    [mode]
+  )
+
   return (
-    <AnimeLayout>
-      <Component {...pageProps} />
-    </AnimeLayout>
+    <ThemeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AnimeLayout>
+          <Component {...pageProps} />
+        </AnimeLayout>
+      </ThemeProvider>
+    </ThemeContext.Provider>
   )
 }
 
